698e129cafe7216cec89569235bd8cc7513f56a1
Thiago Suzuki <thiagoikenaga@gmail.com>
2025-10-20 00:10:34 -0300

    fix: discomment redux archives

--- src/store/hooks.ts
+++ src/store/hooks.ts
@@ -1,6 +1,6 @@
-// import { useDispatch, useSelector, type TypedUseSelectorHook } from "react-redux"
-// import type { RootState, AppDispatch } from "./index"
+import { useDispatch, useSelector, type TypedUseSelectorHook } from "react-redux"
+import type { RootState, AppDispatch } from "./index"
 
-// // Hooks tipados
-// export const useAppDispatch: () => AppDispatch = useDispatch
-// export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
+// Hooks tipados
+export const useAppDispatch: () => AppDispatch = useDispatch
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,11 @@
-// import { configureStore } from "@reduxjs/toolkit"
-// import { player } from "./slices/player"
+import { configureStore } from "@reduxjs/toolkit"
+import { player } from "./slices/player"
 
-// export const store = configureStore({
-//   reducer: {
-//     player,
-//   },
-// })
+export const store = configureStore({
+  reducer: {
+    player,
+  },
+})
 
-// export type RootState = ReturnType<typeof store.getState>
-// export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
