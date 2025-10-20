926768413a31b5fe5dfe52e2d85bef28d979af5f
Thiago Suzuki <thiagoikenaga@gmail.com>
2025-10-20 19:26:12 -0300

    fix: fix name env

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: import.meta.env.API_JSON || 'http://localhost:5000'
+    baseURL: import.meta.env.VITE_API_JSON || 'http://localhost:5000'
 })
