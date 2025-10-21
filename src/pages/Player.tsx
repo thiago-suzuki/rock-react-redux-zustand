d43b57e1a10dcf9ae2e8652cdcd3bcce96088471
Thiago Suzuki <thiagoikenaga@gmail.com>
2025-10-20 21:23:33 -0300

    feat: add responsive player for mobile

--- src/pages/Player.tsx
+++ src/pages/Player.tsx
@@ -29,7 +29,7 @@ export function Player() {
     }, [currentLesson])
 
     return (
-        <div className="h-screen bg-zinc-950 text-zinc-50 flex justify-center items-center">
+        <div className="h-screen bg-zinc-950 text-zinc-50 flex justify-center items-center p-4">
             <div className="flex w-[1180px] flex-col gap-6">
                 <div className="flex items-center justify-between">
                     <Header />
@@ -40,11 +40,27 @@ export function Player() {
                     </button> */}
                 </div>
 
-                <main className="relative flex overflow-hidden rounded-lg border border-zinc-800 bg-zinc-900 shadow pr-80">
+                <main className="
+                    lg:relative flex flex-col lg:flex-row 
+                    overflow-hidden rounded-lg border border-zinc-800 bg-zinc-900 
+                    lg:pr-80
+                ">
                     <div className="flex-1">
                         <Video />
                     </div>
-                    <aside className="w-80 absolute top-0 bottom-0 right-0 border-l divide-y-2 divide-zinc-900 border-zinc-800 bg-zinc-900 overflow-y-scroll scrollbar-thin scrollbar-track-zinc-950 scrollbar-thumb-zinc-800">
+                    <aside
+                        className="
+                            w-full lg:w-80
+                            lg:absolute lg:bottom-0 lg:top-0 lg:right-0
+                            lg:border-l border-t 
+                            divide-y-2 divide-zinc-900 border-zinc-800 
+                            bg-zinc-900 
+                            overflow-y-scroll 
+                            scrollbar-thin scrollbar-track-zinc-950 scrollbar-thumb-zinc-800 
+                            order-2 lg:order-none
+                            max-h-[50vh] lg:max-h-none
+                        "
+                    >
                         {/* {modules && modules.map((module, index) => {
                             return (
                                 <Module
